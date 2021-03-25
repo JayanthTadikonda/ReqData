85787d5633f30009d98c7a556ef74fa1ee37bd50
JayanthTadikonda <jayanth.tadikonda@gmail.com>
2021-03-25 21:02:42 +0530

    car parked assignment

--- Java_Assignments/CarPark/src/ParkedCarOwnerList.java
+++ Java_Assignments/CarPark/src/ParkedCarOwnerList.java
@@ -1,36 +1,43 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ParkedCarOwnerList {
 
-    ArrayList<ParkedCarOwnerDetails> cars = new ArrayList<>();
+    // ArrayList<ParkedCarOwnerDetails> cars = new ArrayList<>();
+    HashMap<Integer, ParkedCarOwnerDetails> carsParked = new HashMap<>();
 
 
-    public void addNewCar(ParkedCarOwnerDetails car){
-        cars.add(car);
+    public void addNewCar(ParkedCarOwnerDetails car, int token) {
+        carsParked.put(token, car);
     }
 
-    public void removeCar(String name){
+    public void removeCar(int token) {
 
-        for (ParkedCarOwnerDetails m:cars) {
-            if(m.getCarModel().equalsIgnoreCase(name)){
-                int index = m.getCarModel().indexOf(name);
-                cars.remove(index);
+        for (Map.Entry m : carsParked.entrySet()) {
+            if (m.getKey().equals(token)) {
+                carsParked.remove(token);
+                System.out.println("Car with token" + token + " removed");
             } else
-                System.out.println("Car Removed");
+                System.out.println("Car not present");
         }
 
     }
 
-    public void getParkedCarLocation(String name){
-
-        for (ParkedCarOwnerDetails d:cars) {
-            if(d.getOwnerName().equalsIgnoreCase(name)){
-                int index = d.getOwnerName().indexOf(name);
-                System.out.println(d.toString()+ "Parked at " + index + "Position: ");
+    public void getParkedCarLocation(int token) {
+        boolean flag = false;
+        ParkedCarOwnerDetails c = new ParkedCarOwnerDetails();
+        for (Map.Entry m : carsParked.entrySet()) {
+            if (m.getKey().equals(token)) {
+                flag = true;
+                c = carsParked.get(m.getKey());
+                break;
             } else
-                System.out.println("Car not Parked.");
-            break;
+                System.out.println("Car not present");
+
         }
+        if(flag)
+            System.out.println("Car Found at: \n" + c.toString());
     }
 }
